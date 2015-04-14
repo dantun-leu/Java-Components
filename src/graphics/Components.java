1909202fd7abc9be9aaa4af484f1b1de8786ecd2
321650582 <321650582@C-G6BQXC1.tdsb.on.ca>
2015-04-14 10:47:57 -0400

    Few updates

--- src/graphics/Components.java
+++ src/graphics/Components.java
@@ -6,7 +6,7 @@ import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Components extends JFrame {
+public class Components extends JFrame implements ActionListener {
 	// Declare objects
 	JLabel label;
 	JButton button1, button2;
@@ -17,22 +17,29 @@ public class Components extends JFrame {
 		// Set up the frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setBounds(300, 300, 200, 350);
-		
+
 		// Create objects
-		 JPanel content = new JPanel();
-		 label = new JLabel ("This is a label.",JLabel.CENTER);
-		 button1 = new JButton ("Button1");
-		 button2 = new JButton ("Button2"); 
-		 
+		JPanel content = new JPanel();
+		label = new JLabel("This is a label.", JLabel.CENTER);
+		button1 = new JButton("Button1");
+		button2 = new JButton("Button2");
+
 		// Add contents
-		 content.add (label);
-		 content.add (button1);
-		 content.add (button2);
-		 
-		 
+		content.add(label);
+		content.add(button1);
+		content.add(button2);
 
 		// Show the contents
+		this.setContentPane(content);
 		this.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == button1) {
+			label.setText("Button 1 was pressed.");
+		} else if (e.getSource() == button2) {
+			label.setText("Button 2 was pressed.");
+		}
 	}
 
 	public static void main(String[] args) {
